a2d653f431eafb263ea155218195c815156f71e4
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-25 13:51:54 +0100

    FINALIZADA

---
+++ src/test/java/mainAPP/Ta26Ud261ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta26Ud261ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
